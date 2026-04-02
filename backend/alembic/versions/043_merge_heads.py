0a406f5622f418833fc4499cb76dc53f9cfaefb3
Imdraks <ayman.hattab01@gmail.com>
2026-04-02 08:51:46 +0200

    fix: merge alembic heads 020+042

---
+++ backend/alembic/versions/043_merge_heads.py
@@ -0,0 +1,19 @@
+"""Merge heads 020 and 042
+
+Revision ID: 043_merge_heads
+Revises: 020_french_legal_fields, 042_bank_metadata_conn
+Create Date: 2026-04-02
+"""
+
+revision = '043_merge_heads'
+down_revision = ('020_french_legal_fields', '042_bank_metadata_conn')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
